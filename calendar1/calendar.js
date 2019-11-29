bfca8607bc239da6b84c8916ca9235adf56181fd
Anton Dogadailo <supp.all.made@gmail.com>
2019-11-29 10:11:09 +0200

    Add files via upload

--- calendar1/calendar.js
+++ calendar1/calendar.js
@@ -1,20 +1,20 @@
 Date.prototype.daysInMonth = function() {
     return 32 - new Date(this.getFullYear(), this.getMonth(), 32).getDate();
 };
-
 const days = new Date().daysInMonth();
 const rows = days / 7;
 const dayNames = ['ПН', "ВТ", "СР", "ЧТ", "ПТ", "СБ", "ВС"];
+let clickedElem;
+let db = {};
+let eventList = document.querySelector(".eventList");
 
-calendar = document.querySelector(".calendar");
-
-
+const calendar = document.querySelector(".calendar");
 
 function createTable (parent, cols, rows) {
     let table = document.createElement('table');
     for (let i = 0; i < rows; i++) {
         const tr = document.createElement('tr');
-        for (let k = 0; k<cols; k++) {
+        for (let k = 0; k < cols; k++) {
             let td = document.createElement('td');
             tr.appendChild(td);
         }
@@ -29,50 +29,63 @@ function createTable (parent, cols, rows) {
 
 createTable(calendar, 7, rows);
 
-// in proccess
-function getTd () {
-    let arr = document.querySelectorAll('td');
-    for (let i = 1;i <= arr.length; i++) {
-        console.log(arr.length)
-        arr[i].addEventListener('click', function func(){
-            this.innerHTML;
+const arr = document.querySelectorAll('td');
+    for (let i = 0;i <= arr.length -1; i++) {
+        arr[i].addEventListener('click', function() {
+            //arr[i].style.color = "#AB0DED";
+            clickedElem = i;
+            console.log(clickedElem);
         })
     }
-    addEventListener('click', function(){
-            this.target;
-        })
     
+const eventText = document.querySelector('input');
+let text = "";
+eventText.addEventListener("keyup", ()=> {
+    text = eventText.value;
+    //console.log(eventText.value)
+});
+
+const button = document.querySelector(".addbtn");
+button.addEventListener("click",function(){
+    console.log("Вы добавили " + text);
+    eventText.value = "";
+        
+        if (typeof db[clickedElem] !== "undefined") {
+            db[clickedElem].push(text);
+            } else {
+                db[clickedElem] = new Array(text);
+            }
+            
+    console.log(db);
+    });
+let buf;
+for (let i = 0;i <= arr.length -1; i++) {
+    arr[i].addEventListener("click", ()=> {
+        eventList = document.createElement("div");
+        eventList.className = "eventList";
+        document.body.appendChild(eventList);
+        let p = document.createElement('p');
+        for (let key in db) {
+            buf = db[clickedElem];
+            for (let i = 0; i < buf.length; i++) {
+                
+                p.innerText = buf[i];
+                eventList.appendChild(p.cloneNode(true))
+                console.log(buf[i]);
+             }
+        
+        }   
+    })
 }
-getTd ();
+function focusElem() {
 
-class Event {
-    constructor (eventName, data) {
-        this.eventName = eventName;
-        this.data = data;
-    }
-    changeData () {
-        //code
-    }
-
-    deleteEvent() {
-        //code
-    }
-}
-
-class Calendar {
-    showDayEvent () {
-//code
-    }
-
-    showWeakEvent () {
-//code
-    }
-
-    showMonthEvent () {
-//code
-    }
 }
 
 
+
+
+    
+    
+    
 
 
